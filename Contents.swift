2f1aefdbffea79dac5362e12303eba2fd4f588a9
Dylansweaver <dylansweaver@gmail.com>
2017-01-30 00:38:38 -0600

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -1,5 +1,4 @@
 //: Playground - noun: a place where people can play
-
 import Foundation
 import Darwin
 
@@ -20,22 +19,22 @@ var arrayOfInts: [Int] = []
 
 
 
-    varA = Int(arc4random_uniform(10))
-varB = Int(arc4random_uniform(10))
+varA = Int(arc4random_uniform(100))
+varB = Int(arc4random_uniform(100))
 while(varB == varA){
-    varB = Int(arc4random_uniform(10))
+    varB = Int(arc4random_uniform(100))
 }
-    varC = Int(arc4random_uniform(10))
+varC = Int(arc4random_uniform(100))
 while (varC == varA || varC == varB){
-    varC = Int(arc4random_uniform(10))
+    varC = Int(arc4random_uniform(100))
 }
-    varD = Int(arc4random_uniform(10))
+varD = Int(arc4random_uniform(100))
 while (varD == varA || varD == varB || varD == varC){
-    varD = Int(arc4random_uniform(10))
+    varD = Int(arc4random_uniform(100))
 }
-    varE = Int(arc4random_uniform(10))
+varE = Int(arc4random_uniform(100))
 while (varE == varA || varE == varB || varE == varC || varE == varD){
-    varE = Int(arc4random_uniform(10))
+    varE = Int(arc4random_uniform(100))
 }
 
 
@@ -51,35 +50,34 @@ arrayOfInts.append(constC)
 arrayOfInts.append(constD)
 arrayOfInts.append(constE)
 
-
+print("constant 1 =",constA, "|constant 2 =" , constB,"|constant 3 =",constC, "|constant 4 =" , constD, "|constant 5=", constE)
 //print(arrayOfInts.sorted())
 
-
-varF = Int(arc4random_uniform(10))
+varF = Int(arc4random_uniform(100))
 while (varF == varA || varF == varB || varF == varC || varF == varD || varF == varE){
-    varE = Int(arc4random_uniform(10))
+    varE = Int(arc4random_uniform(100))
 }
-varG = Int(arc4random_uniform(10))
+varG = Int(arc4random_uniform(100))
 while (varG == varA || varG == varB || varG == varC || varG == varD || varG == varE || varG == varF){
     
-    varG = Int(arc4random_uniform(10))
+    varG = Int(arc4random_uniform(100))
 }
 
-varH = Int(arc4random_uniform(10))
+varH = Int(arc4random_uniform(100))
 while (varH == varA || varH == varB || varH == varC || varH == varD || varH == varE || varH == varF || varH == varG){
-    varH = Int(arc4random_uniform(10))
+    varH = Int(arc4random_uniform(100))
 }
 
 
-varI = Int(arc4random_uniform(10))
+varI = Int(arc4random_uniform(100))
 while (varI == varA || varI == varB || varI == varC || varI == varD || varI == varE || varI == varF || varI == varG || varI == varH){
-    varI = Int(arc4random_uniform(10))
+    varI = Int(arc4random_uniform(100))
 }
 
 
-varJ = Int(arc4random_uniform(10))
+varJ = Int(arc4random_uniform(100))
 while (varJ == varA || varJ == varB || varJ == varC || varJ == varD || varJ == varE || varJ == varF || varJ == varG || varJ == varH || varJ == varI){
-    varJ = Int(arc4random_uniform(10))
+    varJ = Int(arc4random_uniform(100))
 }
 
 
@@ -88,8 +86,7 @@ arrayOfInts.append(varG)
 arrayOfInts.append(varH)
 arrayOfInts.append(varI)
 arrayOfInts.append(varJ)
-
-
+print("variable 1 =",varF, "|variable 2 =" , varG,"|variable 3 =",varH, "|variable 4 =" , varI, "|variable 5=", varJ)
 arrayOfInts = arrayOfInts.sorted()
 
 
@@ -109,13 +106,8 @@ var dict: [Int:Array<Int>] = [
 
 
 
-print(dict[1]!)
 
-
-
-
-
-
-
+print(dict)
+print("Lowest=",dict[0]!,"Highest=",dict[9]!)
 
 
